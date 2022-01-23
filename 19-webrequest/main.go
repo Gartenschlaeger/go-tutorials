25f5bd427043c95af8527f6aa730968a7ef56881
Gartenschlaeger <mail@kaisnet.de>
2022-01-23 15:23:08 +0100

    add example for webrequest

---
+++ 19-webrequest/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+
+	response, err := http.Get("https://www.kaisnet.de/ip.php")
+	must(err)
+
+	fmt.Println("Status", response.Status)
+	fmt.Println("StatusCode", response.StatusCode)
+	fmt.Println("ContentLength", response.ContentLength)
+	fmt.Println("Header", response.Header)
+	fmt.Println("TLS", response.TLS)
+	fmt.Println("TransferEncoding", response.TransferEncoding)
+	fmt.Println("Proto", response.Proto)
+	fmt.Println("ProtoMajor", response.ProtoMajor)
+	fmt.Println("ProtoMinor", response.ProtoMinor)
+	fmt.Println("Trailer", response.Trailer)
+	fmt.Println("Uncompressed", response.Uncompressed)
+
+	data, err := ioutil.ReadAll(response.Body)
+	must(err)
+
+	fmt.Println("Body", string(data))
+
+}
